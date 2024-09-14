721fb5cb74b229f8cd9481b19e3bc9fd216054d5
Rohit joshi <bhdream0000@gmail.com>
2024-09-14 22:33:14 +0530

    using custom error with return type

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{builder::Str, Parser};
+use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
@@ -6,12 +6,23 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[derive(Debug)]
+struct CustomError(String);
+
+impl std::fmt::Display for CustomError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl std::error::Error for CustomError {}
+
 fn main() -> Result<(), Box<dyn std::error::Error>>{
     let args = Cli::parse();
     let result = std::fs::read_to_string(&args.path);
-    let content = match result{
-        Ok(file_content) => {file_content},
-        Err(error) => { return Err(error.into()); }
+    let content = match result {
+        Ok(file_content) => file_content,
+        Err(error) => return Err(CustomError(format!("Error reading `{}`: {}", args.path.display(), error)).into()),
     };
 
     for line in content.lines(){
