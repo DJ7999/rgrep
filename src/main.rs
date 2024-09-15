4591abf9615a69d918c66fd87d60ef4e6982cb47
Rohit joshi <bhdream0000@gmail.com>
2024-09-15 11:10:24 +0530

    fixed for ;

--- src/main.rs
+++ src/main.rs
@@ -28,6 +28,6 @@ fn find_matches(content : &str, pattern : &str, mut writer: impl std::io::Write)
 #[test]
 fn find_a_match(){
     let mut result=Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
     assert_eq!(result, b"lorem ipsum\n")
 }
